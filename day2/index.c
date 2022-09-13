a635971f2000b64d79a35099a2fea790fcd51634
Sanket Musale <101390828+musalesanket17@users.noreply.github.com>
2022-09-13 22:23:19 +0530

    Upload
    Add day2

---
+++ day2/index.c
@@ -0,0 +1,16 @@
+// Instruction And Opreators
+
+// Q.Write a program if number is devisible by 2 or not
+
+# include<stdio.h>
+
+int main(){
+     
+     int a;
+     printf("Enter a Number");
+     scanf("%d",&a);
+     printf("%d",a%2==0);
+     
+     return 0;
+
+}
