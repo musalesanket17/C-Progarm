3bdbad42ea46696570a7453db2184f2d3dcc1758
Sanket Musale <101390828+musalesanket17@users.noreply.github.com>
2022-09-26 17:38:55 +0530

    Take input from user

---
+++ eay1.2/index.c
@@ -0,0 +1,67 @@
+
+// scanf function taking input from user
+/*
+#include<stdio.h>
+
+int main(){
+// You are only able to take input from user with using %d  %c  %f;
+    int a;
+    int b;
+    printf("Enter Value : ");
+    scanf("%d" , &a);
+    printf("Enter Value :");
+    scanf("%d" , &b);
+
+    printf("%-4d %-6d" , a , b);
+ 
+    return 0;
+
+}
+
+
+
+// Escape sequence ;
+\n     for new line;
+\b     for backspace;
+\t     for new tab; 
+\r     carrige return;
+
+
+#include<stdio.h>
+
+int main(){
+     printf("Good Morning\bg");
+    //  it will backspace one char and  "G after n "  
+
+    // [\t  is used as Tab key on keybord for getting some spaces agter one word bascically used for formatting]
+    
+    // if you want to give some special symbol then use [/ any symbol you want to give]
+    printf("\"Hello Vikas\"");
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main(){
+//   How to gate ASCCI Value;
+    int a='A';
+
+    printf("%c %10d" ,a , a );
+   
+    return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main(){
+
+// Unsiegd and singed charecter;
+
+// for char it unsiged char id 255  & siged is 127
+// for int is 
+
+
+    return 0; 
+}
