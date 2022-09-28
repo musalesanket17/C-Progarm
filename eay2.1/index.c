8636d1c5e5e87e74b71d80f1f4845482edc5b508
Sanket Musale <101390828+musalesanket17@users.noreply.github.com>
2022-09-28 13:11:25 +0530

    scanf

---
+++ eay2.1/index.c
@@ -0,0 +1,12 @@
+// Bitwise Oprator
+/*
+ & = Bitwise And  Its behave like  && 
+ | = Bitwise OR   its behave like  ||
+ ~ = -(n+1)    
+ ^ =Bitwise EXO   if both bits are diffrent then retern one[1] BUt its calculate bit like || oprator
+ << shift last two digit to front and remainig take as a zero
+ EX . 1010   -->  1000
+
+ <<  =  x << n = x * 2^n
+ >>  =  x >> n = x/2^n
+ */
